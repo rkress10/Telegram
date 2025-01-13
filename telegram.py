6eff4677c42aa7589bb4f6f2611e414be1dbdcbd
rkress <rkress@umich.edu>
2025-01-12 22:12:51 -0600

    Push

--- telegram.py
+++ telegram.py
@@ -1,51 +1,156 @@
 from telethon import TelegramClient
-from telethon.tl.types import MessageMediaGeo
+from telethon.tl.types import MessageMediaGeo, MessagePeerReaction
 import json
 import requests
 import validators
 import bs4
 from urllib.parse import urlparse
 from urllib.parse import parse_qs
+import datetime
 
 api_id = "25573622"
 api_hash = "a96beeca7659b9911511d819448cb247"
 client = TelegramClient('anon', api_id, api_hash)
 client.start()
+dataCsv = []
+lastReadMessageId = -1
+firstReadMessageId = -1
 
+#region constants
 #region File constants
 lastMessageFileName = "Database/lastMessageId"
 ggoSightingsFileName = "Database/GGOSightings"
+usersFileName = 'Database/Users'
+#endregion
+_latLongconst = 'latlong'
+_pendingconst = 'pending'
 #endregion
 
 async def main():
     channel = await client.get_entity(-1001221913118)
-    useRealData = input("Update Data?: ")
-    minMessageId = TryGetLastMessageId()
+    updateData = input("Update Data?: ")
+    minMessageId = TryGetLastMessageId(updateData)
     sightingsData = TryLoadSightings()
+    isFirst = True
     if minMessageId == -1:
         return
+    if input("Load Users?: ") == "Load Users":
+        await LoadUsers2(channel)
+    users = LoadUsers()
     async for message in client.iter_messages(channel, min_id = minMessageId):
-        if message.action != None:
+        lastReadMessageId = message.id
+        if isFirst:
+            isFirst = False
+            firstReadMessageId = message.id
+        if message.action != None or message.from_id == None:
             # these are mostly users joining the channel, ignore
             continue
+        print(message)
         if type(message.media) == MessageMediaGeo:
-            a = FormatAppleMaps(message.media.geo)
-            print(f'apple maps: {a} from: {message.from_id.user_id}')
-            #sightingsData[message.from_id.user_id].append(a)
-        elif 'GGO' in message.message:
-            print(message.message)
+            HandleAppleMaps(message, sightingsData)
         elif 'maps.app.goo.gl' in message.message:
-            print(f'google maps: {FormatGoogleMaps(message.message)}')
-    print(sightingsData)
+            HandleGoogleMaps(message, sightingsData)
+        elif 'GGO' in message.message.upper() or 'great grey' in message.message.lower() or 'great gray' in message.message.lower():
+            HandleGGO(message, sightingsData)
+        elif GetUserId(message) in sightingsData:
+            if len(sightingsData[GetUserId(message)][_latLongconst]) > 0:
+                sightingsData[GetUserId(message)][_latLongconst].pop()
+                print('Not Added to CSV 2')
+        #print(sightingsData)
+    print(dataCsv)
+    OutputCSV(users)
+    if updateData:
+        UpdateLastReadMessage(lastReadMessageId)
+
+def OutputCSV(users):
+    f = open(f'CSV/GGOSightings_{firstReadMessageId}_{lastReadMessageId}_{datetime.datetime.now()}', 'w')
+    for row in dataCsv:
+        splitRow = row.split(',')
+        username = ''
+        print(splitRow)
+        if splitRow[2] in users:
+            username = users[splitRow[2]]
+        f.write(f'{datetime.date.today()},{username},"{splitRow[0]}, {splitRow[1]}"')
+        f.write('\n')
+    f.close()
+
+async def LoadUsers2(channel):
+    users = await client.get_participants(channel)
+    f = open(usersFileName, 'w')
+    userMap = {}
+    for user in users:
+        print(user)
+        userMap[str(user.id)] = f'{user.first_name} {user.last_name}'
+    json.dump(userMap, f)
+    f.close()
+
+def LoadUsers():
+    f = open(usersFileName, 'r')
+    users = json.loads(f.read())
+    f.close()
+    return users
+
+def HandleGGO(message, sightingsData):
+    print(message.message)
+    userId = GetUserId(message)
+    if userId not in sightingsData:
+        # Add user to sightings
+        sightingsData[userId] = {}
+        sightingsData[userId][_latLongconst] = []
+        sightingsData[userId][_pendingconst] = True
+    else:
+        if len(sightingsData[userId][_latLongconst]) > 0:
+            AddToCsv(sightingsData[userId][_latLongconst][-1], userId)
+            sightingsData[userId][_latLongconst].pop()
+            sightingsData[userId][_pendingconst] = False
+        else:
+            sightingsData[userId][_pendingconst] = True
+
+def HandleAppleMaps(message, sightingsData):
+    latLong = FormatAppleMaps(message.media.geo)
+    print(f'apple maps: {latLong}')
+    userId = GetUserId(message)
+    if userId in sightingsData and sightingsData[userId][_pendingconst]:
+        AddToCsv(latLong, userId)
+        sightingsData[userId][_pendingconst] = False
+    else:
+        AddToSightingsList(latLong, GetUserId(message), sightingsData)
+
+def HandleGoogleMaps(message, sightingsData):
+    latLong = FormatGoogleMaps(message.message)
+    if latLong == None:
+        return
+    print(f'google maps: {latLong}')
+    userId = GetUserId(message)
+    if userId in sightingsData and sightingsData[userId][_pendingconst]:
+        AddToCsv(latLong, userId)
+        sightingsData[userId][_pendingconst] = False
+    else:
+        AddToSightingsList(latLong, GetUserId(message), sightingsData)
+
+def AddToCsv(latLong, userId):
+    dataCsv.append(f'{latLong},{userId}')
+    print('Added to CSV')
+
+def AddToSightingsList(latLong, userId, sightingsData):
+    if userId not in sightingsData:
+        sightingsData[userId] = {}
+        sightingsData[userId][_latLongconst] = []
+        sightingsData[userId][_pendingconst] = False
+    sightingsData[userId][_latLongconst].append(latLong)
+
+def GetUserId(message):
+    return message.from_id.user_id
 
 def FormatAppleMaps(geo):
     return FormatLatLong(geo.lat, geo.long)
 
 def FormatGoogleMaps(url):
+    originalUrl = url
     if url[:3] == 'See':
         url = url[35:]
     if not validators.url(url):
-        print(f'Invalid URL: {url}')
+        print(f'Invalid URL: {originalUrl}')
         return
     x = requests.get(url)
     html = bs4.BeautifulSoup(x.text, features='html5lib')
@@ -53,7 +158,7 @@ def FormatGoogleMaps(url):
         latLong = parse_qs(urlparse(html.title.text).query)['q'][0].split(',')
         return FormatLatLong(latLong[0], latLong[1])
     except:
-        print(f'Could not use URL')
+        print(f'Could not use URL {originalUrl}')
         return
 
 def FormatLatLong(lat, long):
@@ -69,20 +174,24 @@ def TryLoadSightings():
         print("Error loading sightings")
         return {}
 
-def TryGetLastMessageId():
+def TryGetLastMessageId(updateData):
     try:
         f = open(lastMessageFileName, "r")
-        lastReadMessageId = int(f.read())
+        lastReadMessageIds = json.loads(f.read())
         f.close()
-        return lastReadMessageId
+        return lastReadMessageIds["prod"] if updateData=="Y" else lastReadMessageIds["test"]
     except:
         print("Error getting the last read message ID")
         return -1
     
 def UpdateLastReadMessage(lastReadMessageId):
     try:
-        f = open(lastMessageFileName, "w")
-        f.write(lastReadMessageId)
+        f = open(lastMessageFileName, "r")
+        newIds = json.loads(f.read())
+        f.close()
+        newIds["prod"] = lastReadMessageId
+        f.open(lastMessageFileName, "w")
+        f.write(json.dump(newIds, f))
         f.close()
     except:
         print("Problem saving the last read message")
